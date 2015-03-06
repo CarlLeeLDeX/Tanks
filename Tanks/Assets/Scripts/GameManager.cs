f565cb3c9bfe7bb1b9da94a8fe9c7e76d38412a2
Steve Lee <stevehlee@sbcglobal.net>
2015-03-05 19:43:18 -0800

    Added split screen and seperate movements

--- Tanks/Assets/Scripts/GameManager.cs
+++ Tanks/Assets/Scripts/GameManager.cs
@@ -10,20 +10,20 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		player = Random.Range (0, 100);
-		//player = 1;
-		if (player >= 50)
-			player = 2;
-		else
-			player = 1;
-
-		if (player == 1) {
-			cam1.enabled = true;
-			cam2.enabled = false;
-		} else {
-			cam1.enabled = false;
-			cam2.enabled = true;
-		}
+//		player = Random.Range (0, 100);
+//		//player = 1;
+//		if (player >= 50)
+//			player = 2;
+//		else
+//			player = 1;
+//
+//		if (player == 1) {
+//			cam1.enabled = true;
+//			cam2.enabled = false;
+//		} else {
+//			cam1.enabled = false;
+//			cam2.enabled = true;
+//		}
 	}
 	
 	// Update is called once per frame
@@ -38,13 +38,13 @@ public class GameManager : MonoBehaviour {
 
 	public void changeTurn()
 	{
-		cam1.enabled = !cam1.enabled;
-		cam2.enabled = !cam2.enabled;
-		if (player == 1)
-			player = 2;
-		else
-			player = 1;
-		Debug.Log (player);
+//		cam1.enabled = !cam1.enabled;
+//		cam2.enabled = !cam2.enabled;
+//		if (player == 1)
+//			player = 2;
+//		else
+//			player = 1;
+//		Debug.Log (player);
 	}
 
 	public bool getChange() {
--- Tanks/Assets/Scripts/TankController.cs
+++ Tanks/Assets/Scripts/TankController.cs
@@ -20,34 +20,73 @@ public class TankController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (gm1 != null) {
-			if (gm1.getPlayerTurn () == player) {
-				if (Input.GetKeyDown (key)) {
-					Instantiate (bullet, barrel.position, barrel.rotation);
-					meter -= 30;
-				}
-			}
+//		if (gm1 != null) {
+//			if (gm1.getPlayerTurn () == player) {
+//				if (Input.GetKeyDown (key)) {
+//					Instantiate (bullet, barrel.position, barrel.rotation);
+//					meter -= 30;
+//				}
+//			}
+//		}
+//
+//		if (meter <= 0) {
+//			meter = 100;
+//			gm1.changeTurn();
+//		}
+
+		if (Input.GetKeyDown (key)) {
+			Instantiate (bullet, barrel.position, barrel.rotation);
 		}
 
-		if (meter <= 0) {
-			meter = 100;
-			gm1.changeTurn();
-		}
 	}
 
 	void FixedUpdate()
 	{
-		if (gm1 != null) {
+//		if (gm1 != null) {
+//
+//			if (gm1.getPlayerTurn () == player) {
+//				float moveHorizontal = Input.GetAxis ("Horizontal");
+//				print(moveHorizontal);
+//				float moveVertical = Input.GetAxis ("Vertical");
+//				print (moveVertical);
+//
+//				Vector3 movement = new Vector3 (0.0f, 0.0f, moveVertical);
+//				//rigidbody.velocity = movement * movementSpeed;
+//
+//				transform.Rotate (Vector3.up * moveHorizontal, Space.Self);
+//				transform.Translate (movement * movementSpeed);
+//			}
+//		}
 
-			if (gm1.getPlayerTurn () == player) {
-				float moveHorizontal = Input.GetAxis ("Horizontal");
-				float moveVertical = Input.GetAxis ("Vertical");
-
-				Vector3 movement = new Vector3 (0.0f, 0.0f, moveVertical);
-				//rigidbody.velocity = movement * movementSpeed;
-
-				transform.Rotate (Vector3.up * moveHorizontal, Space.Self);
-				transform.Translate (movement * movementSpeed);
+		if (player == 1) {
+			if (Input.GetKey (KeyCode.W)) {
+				Vector3 movement = new Vector3 (0.0f, 0.0f, 1.0f);
+				transform.Translate (movementSpeed * movement);
+			}
+			if (Input.GetKey (KeyCode.S)) {
+				Vector3 movement = new Vector3 (0.0f, 0.0f, -1.0f);
+				transform.Translate (movementSpeed * movement);
+			}
+			if (Input.GetKey (KeyCode.A)) {
+				transform.Rotate (Vector3.up * -1.0f, Space.Self);
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				transform.Rotate (Vector3.up * 1.0f, Space.Self);
+			}
+		} else {
+			if (Input.GetKey ("up")) {
+				Vector3 movement = new Vector3 (0.0f, 0.0f, 1.0f);
+				transform.Translate (movementSpeed * movement);
+			}
+			if (Input.GetKey ("down")) {
+				Vector3 movement = new Vector3 (0.0f, 0.0f, -1.0f);
+				transform.Translate (movementSpeed * movement);
+			}
+			if (Input.GetKey ("left")) {
+				transform.Rotate (Vector3.up * -1.0f, Space.Self);
+			}
+			if (Input.GetKey ("right")) {
+				transform.Rotate (Vector3.up * 1.0f, Space.Self);
 			}
 		}
 
